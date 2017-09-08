d4d2ade1f8dbfddbc3eeedffc6e3d1c82a612c12
King-of-Infinite-Space <fcbarcelona.plh@gmail.com>
2017-09-08 16:49:12 +0800

    keras is good
    hahaha

--- keras_test1.py
+++ keras_test1.py
@@ -1,15 +1,12 @@
 # LSTM and CNN for sequence classification
 import numpy
-from keras.datasets import imdb
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers.convolutional import Conv1D
-from keras.layers.convolutional import MaxPooling1D
-from keras.layers.embeddings import Embedding
+from keras.layers import Dense, Dropout, LSTM
+from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.preprocessing import sequence
+import pandas as pd
 # fix random seed for reproducibility
-numpy.random.seed(7)
+#numpy.random.seed(7)
 
 def standardize_data(array):
     #takes in 2d arrays
@@ -34,6 +31,8 @@ prediction_set=pd.read_csv(TESTDIR, skipinitialspace=True, skiprows=0, usecols=p
 #fea_col = training_set[...,:88]
 #label_col = [...,-1]
 
+print('Datasets Loaded.')
+
 cut = math.floor(all_set.shape[0]*0.7)
 X_train=all_set[0:cut, :88]
 y_train=all_set[0:cut, -1]
@@ -45,22 +44,62 @@ y_test = all_set[cut:, -1]
 X_train=standardize_data(X_train)
 X_test=standardize_data(X_test)
 
+'''
+X_train=np.expand_dims(X_train,axis=2)
+X_test=np.expand_dims(X_test,axis=2)
+X_predict=np.expand_dims(prediction_set,axis=2)
+'''
+
+print('Data prepared.')
+
+
 # create the model
 model = Sequential()
-model.add()
-model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+
+'''
+model.add(Conv1D(input_shape=(None,88),filters=32, kernel_size=3, padding='same', activation='relu'))
 model.add(MaxPooling1D(pool_size=2))
-model.add(LSTM(50))
+'''
+
+'''
+model.add(LSTM(40,input_shape=(88,1),return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(20))
+model.add(Dropout(0.2))
+'''
+
+model.add(Dense(80,input_shape=(88,),activation='selu'))
+#keras.layers.normalization.BatchNormalization()
+model.add(Dropout(0.2))
+
+model.add(Dense(40,activation='selu'))
+#keras.layers.normalization.BatchNormalization()
+model.add(Dropout(0.2))
+
+#Output layer
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
 
+model.fit(X_train, y_train, epochs=50, batch_size=5000, validation_data=(X_test,y_test), sample_weight=training_weight) #not sure whether validation_split uses weight
 
-model.fit(X_train, y_train, epochs=1, batch_size=1000, validation_data=(X_test,y_test), sample_weight=training_weight) #not sure whether validation_split uses weight
+'''
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
+'''
+
+predictions = model.predict(X_predict).flatten()
+print('Max =' + str(np.max(predictions)))
+print('Min =' + str(np.min(predictions)))
+
+#testdata: 321674 ~ 521619
+indices = pd.read_csv(TESTDIR, skipinitialspace=True, skiprows=0, usecols=[0]).as_matrix().flatten()
+df = pd.DataFrame(data={'id':indices, 'proba':predictions})
+df.to_csv('result_notstan_duo.csv',index=False)
+print('Result saved.')
+
 
 #model.save('test_1.h5')  # creates a HDF5 file 'my_model.h5'
 #del model  # deletes the existing model
